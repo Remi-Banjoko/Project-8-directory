ca9ba3e9bcf67460d3f6c55f8cbdc9e5a8732351
Remi-Banjoko <medlyrem@gmail.com>
2022-09-15 14:28:31 -0500

    Finished!!!

---
+++ app.js
@@ -0,0 +1,22 @@
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+
+let msgBtn = document.getElementById("msg-btn")
+
+msgBtn.onclick = function(){
+
+    alert("Your Message has been sent!");
+
+}
